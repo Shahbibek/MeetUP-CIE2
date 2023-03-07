8504be656a9d9bdcb5cae967de854449bd02841e
Bibek Kumar Sah <bkumarsah627@rku.ac.in>
2023-03-07 23:49:40 +0530

    Project created successfully

---
+++ MeetUp/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeetUp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Registration()
+        {
+            return View();
+        }
+    }
+}
